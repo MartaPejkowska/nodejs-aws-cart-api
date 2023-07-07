fd4e0ee5f6f321ffcb40612980a67f345542009f
MartaPejkowska <marta.pejkowska@coderscrew.pl>
2023-07-07 09:47:19 +0200

    deploy

---
+++ cdk-nest.ts
@@ -0,0 +1,33 @@
+import 'source-map-support/register';
+import { App, Stack } from 'aws-cdk-lib';
+import { NodejsFunction } from 'aws-cdk-lib/aws-lambda-nodejs';
+import { Runtime } from "aws-cdk-lib/aws-lambda";
+import { config as envConfig } from 'dotenv';
+
+envConfig();
+
+class ServerStack extends Stack {
+  constructor() {
+
+    super(new App(), `nodejs-cart-api-stack` );
+
+    new NodejsFunction(
+      this,
+      `nodejs-cart-api-express-server-lambda`, {
+        entry: './dist/main.js',
+        runtime: Runtime.NODEJS_18_X,
+        functionName: 'expressServerHandler',
+        handler: 'handler',
+        environment: {
+          PGDATABASE: process.env.PGDATABASE!,
+          PGHOST: process.env.PGHOST!,
+          PGPORT: process.env.PGPORT!,
+          PGUSER: process.env.PGUSER!,
+          PGPASSWORD: process.env.PGPASSWORD!
+        }
+      }
+    );
+  }
+}
+
+new ServerStack();
---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+
+const TerserPlugin = require('terser-webpack-plugin');
+
+module.exports = (options, webpack) => {
+    const lazyImports = [
+      '@nestjs/microservices/microservices-module',
+      '@nestjs/websockets/socket-module',
+    ];
+
+    return {
+      ...options,
+      externals: [],
+      plugins: [
+        ...options.plugins,
+        new webpack.IgnorePlugin({
+          checkResource(resource) {
+            if (!lazyImports.includes(resource)) {
+              return false;
+            }
+            try {
+              require.resolve(resource, {
+                paths: [process.cwd()],
+              });
+            } catch (err) {
+              return true;
+            }
+            return false;
+          },
+        }),
+      ],
+    };
+  };
+
